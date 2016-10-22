ffeefb934d5cb96c74a28a27356ef120681342ad
Praveenkumar <pravinarr@gmail.com>
2016-10-22 00:26:39 -0700

    updating entity classes

--- fiiplswebservice/src/main/java/edu/fiipls/entities/JobEntity.java
+++ fiiplswebservice/src/main/java/edu/fiipls/entities/JobEntity.java
@@ -1,9 +1,11 @@
 package edu.fiipls.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class JobEntity {
 
 	@Id
